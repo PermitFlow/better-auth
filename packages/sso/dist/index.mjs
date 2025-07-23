05ac8fc58bebda341eaf6a65563f030aeed869ce
William Lindsay <william.fr.lindsay@gmail.com>
2025-07-22 21:19:13 -0400

    Trying something

--- packages/sso/dist/index.mjs
+++ packages/sso/dist/index.mjs
@@ -1008,6 +1008,7 @@ const sso = (options) => {
           }
           let session = await ctx.context.internalAdapter.createSession(user.id, ctx);
           await setSessionCookie(ctx, { session, user });
+          console.log(`I SET THE SESSION FOR ${user.email}`);
           return ctx.json({
             redirect: true,
             url: RelayState || `${parsedSamlConfig.issuer}`,
